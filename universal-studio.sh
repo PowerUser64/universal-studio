8b98b8a09690d609ba1689c0d85b1cbbae8f9737
PowerUser64 <blake@blakenorth.net>
2022-10-24 23:57:10 -0700

    universal-studio: allow passing arguments to apps

--- universal-studio.sh
+++ universal-studio.sh
@@ -53,11 +53,11 @@ dbg() { ("${DEBUG:-false}" && err "$@") || true; }
 
 usage() {
    msg "Usage:"
-   msg " Launch applications: $program_name ${Grn}app_1${Nc} ${Grn}app_2${Nc} ${Grn}app_3${Nc} ${Grn}[...]${Nc}"
-   msg " Other functionality: $program_name ${Grn}[option]${Nc}"
+   msg " Launch applications: $program_name ${Grn_o}app_1${Nc_o} ${Grn_o}app_2${Nc_o} ${Grn_o}app_3${Nc_o} ${Grn_o}[...]${Nc_o}"
+   msg " Other functionality: $program_name ${Grn_o}[option]${Nc_o}"
    msg "  The options are:"
-   msg "   ${Grn}list${Nc}             List all applications available to launch"
-   msg "   ${Grn}help${Nc}             Print this help menu"
+   msg "   ${Grn_o}list${Nc_o}             List all applications available to launch"
+   msg "   ${Grn_o}help${Nc_o}             Print this help menu"
    msg
    msg "Example:"
    msg " $program_name bespokesynth ardour carla     # Start bespokesynth, ardour, and carla"
@@ -72,6 +72,11 @@ version() {
    exit "$1"
 }
 
+# Print the first argument
+get_name() { printf '%s' "$1"; }
+# Print all arguments but the first argument
+get_args() { shift; if test $# -gt 0; then printf '%s ' "$@"; fi; }
+
 # Extract the list of applications from flake.nix
 pkgs_list_available() {
    # Download the
@@ -84,7 +89,8 @@ pkgs_list_available() {
 pkgs_is_available() {
    all_pkgs="$(pkgs_list_available)"
    while ! test $# = 0; do
-      echo "$all_pkgs" | grep -Fx "$1" > /dev/null || printf '  %s\n' "$1"
+      search="$(eval "get_name $1")"
+      echo "$all_pkgs" | grep -Fx "$search" > /dev/null || printf '  %s\n' "$search"
       shift
    done
 }
@@ -96,39 +102,39 @@ am_i_root() { test "$(id -u)" = 0; }
 command_exists() { command -v "$1" > /dev/null; }
 
 # Get some colors for ~flare~
-Red='' Ylw='' Nc='' Grn='' Nc='';:
+Red_e='' Grn_o='' Grn_e='' Ylw_e='' Cyn_o='' Nc_o='' Nc_e=''
 # Colors for stdout
 if test -t 1; then
    if command_exists tput; then
-      Grn="$(tput setaf 2)" Nc="$(tput sgr0)";:
+      Grn_o="$(tput setaf 2)" Cyn_o="$(tput setaf 6)" Nc_o="$(tput sgr0)";:
    else
-      Grn='[32m' Nc='(B[m';:
+      Grn_o='[32m' Cyn_o='[36m' Nc_o='(B[m';:
    fi
 fi
 # Colors for stderr
 if test -t 2; then
    if command_exists tput; then
-      Red="$(tput setaf 1)" Ylw="$(tput setaf 3)" Nc="$(tput sgr0)";:
+      Red_e="$(tput setaf 1)" Grn_e="$(tput setaf 2)" Ylw_e="$(tput setaf 3)" Nc_e="$(tput sgr0)";:
    else
-      Red='[31m' Ylw='[33m' Nc='(B[m';:
+      Red_e='[31m' Grn_e='[32m' Ylw_e='[33m' Nc_e='(B[m';:
    fi
 fi
 
 # Warn if running as root
 if am_i_root; then
-   err "${Ylw}Warning:${Nc} This script does not need to be run as root."
+   err "${Ylw_e}Warning:${Nc_e} This script does not need to be run as root."
    sleep 2
 fi
 
 # Test if curl exists
 if ! command_exists curl; then
-   err "${Red}Error:${Nc} \`curl\` does not exist or could not be found in $PATH. Please install curl and try again." >&2
+   err "${Red_e}Error:${Nc_e} \`curl\` does not exist or could not be found in $PATH. Please install curl and try again." >&2
    exit 1
 fi
 
 # Check if no arguments are passed
 if test $# = 0; then
-   err "${Red}Error:${Nc} Please provide an option or a list or applications."
+   err "${Red_e}Error:${Nc_e} Please provide an option or a list or applications."
    usage 1
 fi
 
@@ -138,15 +144,15 @@ case "$1" in
    -h|--help|help) usage 0;;
    -v|--version|version) version 0;;
    # Don't allow packages that start with a '-' so grep doesn't complain
-   -*) err "${Red}Error:${Nc} option $1 does not exist."; err "See \`$program_name help\` for a list of all options."; exit 1;;
+   -*) err "${Red_e}Error:${Nc_e} option $1 does not exist."; err "See \`$Grn_e$program_name help$Nc_e\` for a list of all options."; exit 1;;
 esac
 
 # Exit if any unavailable applications were requested
 unavailable="$(pkgs_is_available "$@")"
 if [ -n "$unavailable" ]; then
-   msg "${Red}Error:${Nc} Some of the requested packages are not available:"
-   msg "$unavailable"
-   msg "See $program_name --list for a list of all available applications"
+   err "${Red_e}Error:${Nc_e} Some of the requested packages are not available:"
+   err "$unavailable"
+   err "See $Grn_e$program_name --list$Nc_e for a list of all available applications"
    exit 1
 fi
 
@@ -154,32 +160,30 @@ fi
 if command_exists nix && ! "$FORCE_NIX_PORTABLE"; then
    dbg '`nix` command detected, using it'
    nix='nix --extra-experimental-features flakes --extra-experimental-features nix-command'
-
 elif command_exists nix-portable; then  # check if nix-portable is in $PATH
    nix="nix-portable nix"
-
 # Check if we haven't yet downloaded nix-portable
 elif ! test -x "$nix_portable_location"; then
    # Get nix-portable from GitHub releases
-   msg '`nix-portable` not found locally, downloading from github releases...'
+   msg '`${Grn_o}nix-portable${Grn_e}` not found locally, downloading from github releases...'
    curl -sSLo "$nix_portable_location" "$nix_portable_dl_url"
    chmod +x "$nix_portable_location"
    nix="$nix_portable_location nix"
-
 # If we already have nix-portable and it's executable, it will get here
 else
    nix="$nix_portable_location nix"
-
 fi
-
 if "$FORCE_NIX_PORTABLE"; then
    dbg "Forcing nix portable"
 fi
 
 dbg "nix command is $nix"
-for package in "$@"; do
-   msg "Running ${Grn}$package${Nc} with nix…"
+while test $# -gt 0; do
+   pkg="$(eval "get_name $1")"
+   args="$(eval "get_args $1")"
+   msg "Running ${Grn_o}$pkg ${Cyn_o}$args${Nc_o} with ${Grn_o}$(eval "get_name $nix")${Nc_o}…"
    # arguments are quoted here because zsh doesn't like having the pound sign unescaped in some cases
-   eval "$nix run '$flake#$package'" &
+   eval "$nix run '$flake#$pkg' -- $args" &
+   shift
 done
 wait
